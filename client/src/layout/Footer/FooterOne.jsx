5730745072ffe1fa83ebf34576084ff17238b021
Nikolay <nikolay@chromeye.com>
2023-12-23 18:09:03 +0200

    make a post request to the lambda function with the email submitted in the form

--- client/src/layout/Footer/FooterOne.jsx
+++ client/src/layout/Footer/FooterOne.jsx
@@ -1,66 +1,37 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { LambdaClient, InvokeCommand, LogType } from "@aws-sdk/client-lambda"; // ES Modules import
+import React from "react";
 import { Button, Form, Input, notification } from "antd";
-const { fromTemporaryCredentials } = require("@aws-sdk/credential-providers");
 
 const FooterOne = ({ footerLight, style, footerGradient }) => {
   const [api, contextHolder] = notification.useNotification();
   const [form] = Form.useForm();
-  const AWS = require("aws-sdk");
-
-  AWS.config.update({
-    accessKeyId: "YOURKEY",
-    secretAccessKey: "YOURSECRET",
-  });
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const credentials = await fromTemporaryCredentials({
-      params: {
-        RoleArn: "arn:aws:lambda:us-east-1:401532472896:function:email-submit",
-      },
-      clientConfig: {
-        region: "us-west-2",
-      },
-    })();
-
-    try {
-      const client = new LambdaClient({
-        region: "us-west-2",
-        credentials,
-      });
-
-      const command = new InvokeCommand({
-        FunctionName: "email-submit",
-        Payload: JSON.stringify("payload"),
-        LogType: LogType.Tail,
-      });
-
-      const { Payload, LogResult } = await client.send(command);
-      const result = Buffer.from(Payload).toString();
-      const logs = Buffer.from(LogResult, "base64").toString();
-      return { logs, result };
-    } catch (error) {
-      console.error("Error invoking function:", error);
-      // Handle errors as needed
-    }
-  };
 
   const handleFormSubmit = async (formValues) => {
-    console.log(formValues);
+    const { email } = formValues;
 
     try {
+      const result = await fetch(
+        "https://s7mvanfyy7726dco5yzt3emmsq0zeswn.lambda-url.us-east-1.on.aws/",
+        {
+          method: "POST",
+          body: {
+            email: email,
+          },
+        }
+      );
+
+      console.log(result);
+
       api.success({ message: "Form was submited successfully" });
       form.resetFields();
-    } catch (error) {}
+    } catch (error) {
+      api.error({ message: error.message });
+      form.resetFields();
+    }
   };
 
   return (
     <>
       {contextHolder}
-      CONTROLLED FORM
       <Form onFinish={handleFormSubmit} className="form" form={form}>
         <Form.Item
           name={"email"}
@@ -79,26 +50,6 @@ const FooterOne = ({ footerLight, style, footerGradient }) => {
           Subscribe
         </Button>
       </Form>
-      OLD FORM
-      <form
-        className="newsletter-form position-relative d-block d-lg-flex d-md-flex"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          className="input-newsletter form-control me-2"
-          placeholder="Enter your email"
-          name="email"
-          required={true}
-          autoComplete="off"
-        />
-        <input
-          type="submit"
-          value="Subscribe"
-          data-wait="Please wait..."
-          className="btn btn-primary mt-3 mt-lg-0 mt-md-0"
-        />
-      </form>
     </>
   );
 };
